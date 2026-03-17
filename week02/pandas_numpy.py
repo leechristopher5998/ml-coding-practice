e41b6790e4a0d1d0d8d44d11fcd7c932cdddb777
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 10:50:00 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -129,5 +129,8 @@ print(result)
 
 # 예제 데이터 생성
 data = {
-    'name' : ['Alice', 'Bob', 'Charlie', 'David'],
+    'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'math' : [88 ,92 , 85, 95 ,90],
+    'science' : [80, 85, 88, 92, 85],
+    'english' : [90, 87, 85, 93, 88]
 }
