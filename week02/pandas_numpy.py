6565788d219958741d53875154e5688abb589797
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 11:12:30 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -203,5 +203,12 @@ print(np.random.randint(50,71, 10))
 print(np.random.choice(['red', 'green','white','black','blue'],size=3))
 print(np.random.choice(['red', 'green','white','black','blue'],size=3, replace=False))
 
-print(np.arange())
+print(np.arange(1,11))
+print(np.arange(1,11,2))
+print(np.arange(1,100,10))
+
+A = np.array([4, 16, 25])
+print(np.sqrt(A))
+
+print()
      
