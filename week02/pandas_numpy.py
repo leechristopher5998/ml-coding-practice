d16a035524a7ab7def97e566951f058006e75fd1
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 10:45:00 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -108,4 +108,11 @@ print(iris.drop('class',axis=1).agg(['sum', 'mean', 'std']))
 
 # print(iris.agg(['sum','mean', 'std']))    # class 컬럼으로 인해 에러 발생
 
-print(iris['class'])
+print(iris['class'].unique())
+print(iris['class'].nunique())
+print(iris['class'].value_counts())
+
+# 예제 데이터 생성
+data = {
+    'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah'],
+}
