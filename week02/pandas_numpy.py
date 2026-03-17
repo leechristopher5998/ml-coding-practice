33a743b21c884470af708964c9932ecace32319f
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 10:41:30 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -96,3 +96,5 @@ print(sample_df.set_index('var_1'))
 print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
+
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
