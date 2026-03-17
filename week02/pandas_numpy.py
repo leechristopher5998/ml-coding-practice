9dd7bcbfab22f5a36c9f2fc49c2c45d9b423bcf6
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 11:07:00 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -180,3 +180,5 @@ print(pd.DataFrame(sample_np, columns=sample_df.columns))    # 새 프레임워�
 print(sample_np)
 print(sample_np[0])
 print(sample_np[0,2])
+print(sample_np[0:3, 2:4])
+print(sample_np[:, 2:4])
