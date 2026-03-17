a4d04da843c847f0ef9f4e832feb206497cf833e
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-17 09:28:55 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -36,4 +36,6 @@ print(sample_df.loc['a':'c'])
 # iloc: integer location의 약자
 print(sample_df.iloc[[0,1,2]])
 print(sample_df.iloc[0:2])
-print(sample_df.iloc[0:5:2]) # 0부터 5까지 2 간격으로 가져오기
+print(sample_df.iloc[0:3]) 
+print(sample_df.iloc[0:3, 2:4]) # 컬럼까지 동시에 인덱싱
+
