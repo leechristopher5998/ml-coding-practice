1161d75c6ef9d6a2e6ad32d230d5ba549441bf74
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-26 13:43:07 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -6,3 +6,11 @@ import pandas as pd
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
 # head( ) 함수를 출력하여 타이타닉 데이터셋의 구성을 간단히 살펴보기
+
+# 데이터 처음 5개의 행 출력
+print(titanic.head())
+
+# 열에 대한 요약 정보 확인
+print(titanic.info())
+
+"""### **선 그래프 : 객실 등급에 따른 생존율 표시하기**"""
