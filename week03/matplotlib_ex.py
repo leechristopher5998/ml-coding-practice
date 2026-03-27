e72acee208f2d548d2a6f79fb4f54672ce9a4eb2
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-27 13:55:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -52,3 +52,9 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 # 생존자 수 표시
 for i, value in enumerate(survived_counts):
     plt.text(i, value + 1, str(value), ha='center', va='bottom')
+
+plt.savefig('Figure02.png')
+plt.close()
+
+"""### **수평 막대 그래프 : 성별에 따른 생존자 수 확인하기**"""    
+    
