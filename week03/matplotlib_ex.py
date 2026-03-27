98b4ab4ced772715f1b842f51b5cffb3daa64bc8
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-27 13:46:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -29,4 +29,10 @@ plt.title('Survival Rate Variation Across Passenger Classes')
 plt.xlabel('Pclass')
 plt.ylabel('Survival Rate')
 plt.grid(True)
-plt.savefig('Figure01.png')
+plt.savefig('Figure01.png')       # 결과를 그림파일로 저장
+plt.close()                       # 다음 Plot을 새로 그리기 위해 plt 닫기
+
+"""### **수직 막대 그래프 : 각 승선 항구에 따른 생존자 수 확인하기**"""
+
+# 승선 항구에 따른 생존자의 수 계산
+survived_counts
