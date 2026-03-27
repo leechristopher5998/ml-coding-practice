82a26502e532c10ace1b9a41ab2f90cc6d1dd161
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-28 08:25:48 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -165,3 +165,6 @@ age_groups = pd.cut(titanic['Age'], bins=range(0, 81, 5))
 
 # Age, Survived 기준으로 그룹화
 survived_counts = titanic.groupby([age_groups, 'Survived'], observed=False).size().unstack().fillna(0)
+print(survived_counts)
+
+# 영역 채우기 그래프 그리기
