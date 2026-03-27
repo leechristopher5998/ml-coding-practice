f9310e9aaa387f444d3886e5c3751aed07b1e24b
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-03-27 14:06:06 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -95,3 +95,8 @@ plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
 plt.legend(handles=scatter.legend_elements()[0], title='Survived',
+           labels=['Not Survived', 'Survived'], loc='upper right')
+plt.savefig('Figure04.png')
+plt.close()
+
+"""### **파이 차트 : 생존자, 사망자 비율 표현하기**"""
