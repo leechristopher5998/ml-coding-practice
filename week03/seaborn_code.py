31d797938a720acc8684f828e01c724d10d25d28
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-04-05 05:11:28 +0900

    Updated Changes

--- week03/seaborn_code.py
+++ week03/seaborn_code.py
@@ -17,3 +17,9 @@ import matplotlib.pyplot as plt
 fig = plt.figure(figsize=(15, 5))
 ax1 = fig.add_subplot(1, 2, 1)  
 ax2 = fig.add_subplot(1, 2, 2)
+
+# stripplot( ) 그리기
+sns.stripplot(x='day', y='total_bill', data=tips, ax=ax1)
+
+# swarmplot( ) 그리기
+sns.swarmplot(x='day', y='total_bill', data=tips, ax=ax2)
