5f1ad4cd3c7239d3272b44624f44d7b47f19b75e
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-04-05 05:08:58 +0900

    Updated Changes

--- week03/seaborn_code.py
+++ week03/seaborn_code.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # 사본 라이브러리 불러오기
-import seaborn ZipFile The class for reading and writing ZIP files.  See section 
+import seaborn as sns
+
+# **팁(tips) 데이터셋 불러오기**
+tips = sns.load_dataset('tips')
+print(tips.head())
+
+tips.info()
+
+# **범부형 변수 산정도 그래프**
+
+
