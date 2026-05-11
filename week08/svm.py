428a570ad150f757201326e1b9c3f38c1cccbaf9
DESKTOP-3TOIUUS\leech <leechristopher5998@gmail.com>
2026-05-11 21:05:15 +0900

    Auto commit: 2026-05-11 21:05 - 저장됨

--- week08/svm.py
+++ week08/svm.py
@@ -13,7 +13,7 @@ y = (iris.target == 2)  # Iris virginica
 
 svm_clf = make_pipeline(StandardScaler(),
                         LinearSVC(C=1, dual=True, random_state=42))
-svm_clf.flt(X,y)
+svm_clf.fit(X,y)
 
 X_new = [[5.5, 1.7], [5.0, 1.5]]
 
